3e838ce09dd68e0a2caaad069350244cb68c521d
Administrator <Administrator@PC-201906132254>
2019-07-11 00:15:40 +0800

    wew

--- ChuangfengDesktop/AddFundDetailWidget.cpp
+++ ChuangfengDesktop/AddFundDetailWidget.cpp
@@ -1,8 +1,12 @@
 #include "AddFundDetailWidget.h"
 #include "globalVariable.h"
 #include <QCalendarWidget>
-
-
+#include "AddMaterialWidget.h"
+#include <QThread>
+#include "SingletonHttpRequest.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include "AddFeedStoreWidget.h"
 
 AddFundDetailWidget::AddFundDetailWidget(PopFundDetailEnum type, QWidget *parent)
 	:MoveableFramelessWindow(parent)
@@ -64,6 +68,65 @@ QWidget* AddFundDetailWidget::getDragnWidget()
 	return ui->pop_widget_title;
 }
 
+void AddFundDetailWidget::SlotThreadAddFeedStore()
+{
+	QString strParam = QString("operat_time=%1&supplier=%2&subject_name=%3&specs=%4&price=%5&unit=%6&number=%7")
+		.arg(m_time).arg(m_suplier).arg(m_subject_name).arg(m_specs).arg(m_price).arg(m_unit).
+		arg(m_number);
+	QByteArray responseData;
+	SingletonHttpRequest::getInstance()->RequestPost("http://127.0.0.1:80/zerg/public/index.php/InComFeedStore"
+		, TempToken, strParam, responseData);
+	QJsonParseError json_error;
+	QJsonDocument parse_doucment = QJsonDocument::fromJson(responseData, &json_error);
+	if (json_error.error == QJsonParseError::NoError)
+	{
+		if (parse_doucment.isArray())
+		{
+			QString time = ui->dateEdit->text();
+			QString strInCom = ui->incom_Edit->text();
+			QString strPay = ui->pay_Edit->text();
+			QString remake = ui->remake_Edit->text();
+			QString taskName;
+			QString costArea;
+
+			taskName = ui->subject_comboBox->currentText();
+
+			if (ui->share_comboBox->currentText() == QString::fromLocal8Bit("自定义"))
+			{
+				costArea = ui->share_Edit->text();
+			}
+			else
+			{
+				costArea = ui->share_comboBox->currentText();
+			}
+			if (costArea == "" || taskName == "" || strPay == "" || strInCom == "")
+			{
+
+				ui->error_lab->setVisible(true);
+				return;
+			}
+			else {
+				emit sig_comit(time, strInCom, strPay, taskName, costArea, remake);
+				emit ui->pop_close_btn->clicked();
+			}
+		}
+		else
+		{
+			QJsonObject rootObject = parse_doucment.object();
+			if (!rootObject["error_code"].isNull())//
+			{
+				int errorcode = rootObject["error_code"].toInt();
+				QString strMsg = rootObject["msg"].toString();
+				emit sig_NotifyMsg(strMsg, errorcode);
+			}
+		}
+	}
+	else {
+		int errorcode = 404;
+		emit sig_NotifyMsg(QString::fromLocal8Bit("网络请求异常！"), errorcode);
+	}
+}
+
 void AddFundDetailWidget::SubjectcomboBoxValueChanged()
 {
 	if (ui->subject_comboBox->currentText() == QString::fromLocal8Bit("自定义"))
@@ -88,13 +151,35 @@ void AddFundDetailWidget::SharecomboBoxValueChanged()
 
 void AddFundDetailWidget::SlotCommitAction()
 {
+	if (ui->subject_comboBox->currentText() == QString::fromLocal8Bit("物料"))
+	{
+
+		AddMaterialWidget*pQtWidget = new AddMaterialWidget();
+		connect(pQtWidget, SIGNAL(sig_commit(QString&, QString&, QString&, QString&, QString&, QString&, QString&, QString&, QString&)), this, SLOT(SlotAddMaterialDetail(QString&, QString&, QString&, QString&, QString&, QString&, QString&, QString&, QString&)));
+		pQtWidget->setAttribute(Qt::WA_DeleteOnClose);
+		pQtWidget->setWindowModality(Qt::ApplicationModal);
+		pQtWidget->show();
+		return;
+	}
+
+	if (ui->subject_comboBox->currentText() == QString::fromLocal8Bit("饲料营养品"))
+	{
+		AddFeedStoreWidget*pQtWidget = new AddFeedStoreWidget();
+		connect(pQtWidget, SIGNAL(sig_commit(QString&, QString&, QString&, QString&, QString&, QString&, QString&)), this, SLOT(SlotAddFeedStore(QString&, QString&, QString&, QString&, QString&, QString&, QString&)));
+		pQtWidget->setAttribute(Qt::WA_DeleteOnClose);
+		pQtWidget->setWindowModality(Qt::ApplicationModal);
+		pQtWidget->show();
+
+		return;
+	}
+	
 	QString time = ui->dateEdit->text();
 	QString strInCom = ui->incom_Edit->text();
 	QString strPay = ui->pay_Edit->text();
 	QString remake = ui->remake_Edit->text();
 	QString taskName;
 	QString costArea;
-	if (ui->share_comboBox->currentText() == QString::fromLocal8Bit("自定义"))
+	if (ui->subject_comboBox->currentText() == QString::fromLocal8Bit("自定义"))
 	{
 		taskName = ui->subject_Edit->text();
 	}
@@ -120,4 +205,96 @@ void AddFundDetailWidget::SlotCommitAction()
 		emit sig_comit(time, strInCom, strPay, taskName, costArea, remake);
 		emit ui->pop_close_btn->clicked();
 	}
+}
+
+void AddFundDetailWidget::SlotAddMaterialDetail(QString&time, QString&use, QString&subject_name, QString&category, QString&price, QString&unit, QString&specs, QString&fare, QString&number)
+{
+	m_addTime = time;
+	m_addUse = use;
+	m_addSubject_name = subject_name;
+	m_addCategory = category;
+	m_addPrice = price;
+	m_addUnit = unit;
+	m_addSpecs = specs;
+	m_addFare = fare;
+	m_addNumber = number;
+	QThread *m_pThread = new QThread;
+	connect(m_pThread, SIGNAL(started()), this, SLOT(SlotThreadAddMaterialDetail()));
+	connect(m_pThread, SIGNAL(finished()), m_pThread, SLOT(deleteLater()));
+	m_pThread->start();
+}
+
+void AddFundDetailWidget::SlotThreadAddMaterialDetail()
+{
+	QString strParam = QString("operat_time=%1&use=%2&subject_name=%3&category=%4&price=%5&unit=%6&specs=%7&number=%8&fare=%9")
+		.arg(m_addTime).arg(m_addUse).arg(m_addSubject_name).arg(m_addCategory).arg(m_addPrice).arg(m_addUnit).
+		arg(m_addSpecs).arg(m_addNumber).arg(m_addFare);
+	QByteArray responseData;
+	SingletonHttpRequest::getInstance()->RequestPost("http://127.0.0.1:80/zerg/public/index.php/InsertMaterial"
+		, TempToken, strParam, responseData);
+	QJsonParseError json_error;
+	QJsonDocument parse_doucment = QJsonDocument::fromJson(responseData, &json_error);
+	if (json_error.error == QJsonParseError::NoError)
+	{
+		if (parse_doucment.isArray())
+		{
+			
+			QString time = ui->dateEdit->text();
+			QString strInCom = ui->incom_Edit->text();
+			QString strPay = ui->pay_Edit->text();
+			QString remake = ui->remake_Edit->text();
+			QString taskName;
+			QString costArea;
+			
+			taskName = ui->subject_comboBox->currentText();
+			
+			if (ui->share_comboBox->currentText() == QString::fromLocal8Bit("自定义"))
+			{
+				costArea = ui->share_Edit->text();
+			}
+			else
+			{
+				costArea = ui->share_comboBox->currentText();
+			}
+			if (costArea == "" || taskName == "" || strPay == "" || strInCom == "")
+			{
+
+				ui->error_lab->setVisible(true);
+				return;
+			}
+			else {
+				emit sig_comit(time, strInCom, strPay, taskName, costArea, remake);
+				emit ui->pop_close_btn->clicked();
+			}
+		}
+		else
+		{
+			QJsonObject rootObject = parse_doucment.object();
+			if (!rootObject["error_code"].isNull())//
+			{
+				int errorcode = rootObject["error_code"].toInt();
+				QString strMsg = rootObject["msg"].toString();
+				emit sig_NotifyMsg(strMsg, errorcode);
+			}
+		}
+	}
+	else {
+		int errorcode = 404;
+		emit sig_NotifyMsg(QString::fromLocal8Bit("网络请求异常！"), errorcode);
+	}
+}
+
+void AddFundDetailWidget::SlotAddFeedStore(QString&time, QString&suplier, QString&subject_name, QString&price, QString&unit, QString&specs, QString&number)
+{
+	m_time = time;
+	m_suplier = suplier;
+	m_subject_name = subject_name;
+	m_price = price;
+	m_unit = unit;
+	m_specs = specs;
+	m_number = number;
+	QThread *m_pThread = new QThread;
+	connect(m_pThread, SIGNAL(started()), this, SLOT(SlotThreadAddFeedStore()));
+	connect(m_pThread, SIGNAL(finished()), m_pThread, SLOT(deleteLater()));
+	m_pThread->start();
 }
--- ChuangfengDesktop/AddFundDetailWidget.h
+++ ChuangfengDesktop/AddFundDetailWidget.h
@@ -16,6 +16,7 @@ public:
 	~AddFundDetailWidget();
 private:
 	virtual QWidget*getDragnWidget();
+	
 private:
 	Ui::addFundDetailWidget *ui;
 	PopFundDetailEnum m_enWidgetType;
@@ -23,7 +24,29 @@ private slots:
 	void SubjectcomboBoxValueChanged();
 	void SharecomboBoxValueChanged();
 	void SlotCommitAction();
+	void SlotAddMaterialDetail(QString&time, QString&use, QString&subject_name, QString&category, QString&price, QString&unit, QString&specs, QString&fare, QString&number);
+	void SlotThreadAddMaterialDetail();
+	void SlotAddFeedStore(QString&time, QString&suplier, QString&subject_name, QString&price, QString&unit, QString&specs, QString&number);
+	void SlotThreadAddFeedStore();
 signals:
 	void sig_comit(QString&time, QString &incom, QString&pay, QString&taskName, QString&shareName, QString&remake);
+private:
+	QString m_addTime;
+	QString m_addUse;
+	QString m_addSubject_name;
+	QString m_addCategory;
+	QString m_addPrice;
+	QString m_addUnit;
+	QString m_addSpecs;
+	QString m_addFare;
+	QString m_addNumber;
+
+	QString m_time;
+	QString m_suplier;
+	QString m_subject_name;
+	QString m_price;
+	QString m_unit;
+	QString m_specs;
+	QString m_number;
 };
 
